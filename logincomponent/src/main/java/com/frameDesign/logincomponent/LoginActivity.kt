cc7b2e6cc1a0b8b85d7e1182795ada6e0ffc1fda
李先生 <liyong991581716@163.com>
2019-10-17 16:30:37 +0800

    login组件模块初始化提交

---
+++ logincomponent/src/main/java/com/frameDesign/logincomponent/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.frameDesign.logincomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.logincomponent_activity_login)
+    }
+}

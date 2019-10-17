929b874ba317edecb6b0924e9d2813890e7ed3a9
李先生 <liyong991581716@163.com>
2019-10-17 17:57:54 +0800

    架构基础组件、库目录搭建

---
+++ basecomponent/src/main/java/com/frameDesign/basecomponent/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.frameDesign.basecomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.basecomponent_activity_main)
+    }
+}

40430430a760fcafe3490cf0ce9522816a4684c9
李先生 <liyong991581716@163.com>
2019-10-22 09:32:08 +0800

    配置提交

---
+++ commonreslib/src/main/java/com/frameDesign/commonreslib/const/router/RouterGood.kt
@@ -0,0 +1,12 @@
+package com.frameDesign.baseres.const.router
+
+/**
+ * description
+ * @author liyong
+ * @date 2019-10-21
+ */
+object RouterGood {
+    private val GOOD = "/good"
+
+    val GOOD_ACTIVITY = GOOD + "/good"
+}
